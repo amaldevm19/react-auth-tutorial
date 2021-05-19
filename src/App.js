d13debc5aa59f8d3aa987912e4f3f63e656b6ef5
amaldevm19 <amaldevm19@gmail.com>
2021-05-19 20:15:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+
+import React,  { useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Dashboard from './components/Dashboard';
+import Preferences from './components/Preferences';
+import Login from './components/Login';
+import useToken from './useToken';
+
+import './App.css';
+
+
+function App() {
+  // const [token, setToken] = useState();
+  const { token, setToken } = useToken();
+ 
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+export default App;
